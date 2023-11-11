787fda69c844eb70c20571d0fce3767ed00b30aa
Valentyn Krylovetskyi <valentyn_krylovetskyi@epam.com>
2023-11-11 15:47:09 +0200

    Initial commit

---
+++ src/main/java/com/krylovetskyi/demoinventorysystem/DemoInventorySystemApplication.java
@@ -0,0 +1,13 @@
+package com.krylovetskyi.demoinventorysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoInventorySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoInventorySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krylovetskyi/demoinventorysystem/DemoInventorySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krylovetskyi.demoinventorysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoInventorySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
